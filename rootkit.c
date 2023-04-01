4f02813baa7e7b0f830a32e6a99efe79a22c3e14
Amit Katz <amitkatz2013@gmail.com>
2023-04-01 14:19:44 +0300

    Initial commit

---
+++ rootkit.c
@@ -0,0 +1,21 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Amit Katz");
+MODULE_DESCRIPTION("A simple Linux module");
+MODULE_VERSION("1.0");
+
+static int __init rootkit_init(void)
+{
+    printk(KERN_INFO "Hello World!\n");
+    return 0;
+}
+
+static void __exit rootkit_exit(void)
+{
+}
+
+module_init(rootkit_init);
+module_exit(rootkit_exit);
